7addfba8556e7b8831c39691997969f930639c5e
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2024-06-23 01:27:03 +0300

    Додав composable функцію useAvailableHeight

---
+++ src/composables/useAvailableHeight.js
@@ -0,0 +1,26 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+import { useLayout } from 'vuetify'
+
+export function useAvailableHeight() {
+  const availableHeight = ref(0)
+  const layout = useLayout()
+
+  const calculateAvailableHeight = () => {
+    const headerHeight = layout.main.getRect().top || 0
+    const footerHeight = layout.footer.getRect().height || 0
+    availableHeight.value = window.innerHeight - headerHeight - footerHeight
+  }
+
+  onMounted(() => {
+    calculateAvailableHeight()
+    window.addEventListener('resize', calculateAvailableHeight)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', calculateAvailableHeight)
+  })
+
+  return {
+    availableHeight
+  }
+}
