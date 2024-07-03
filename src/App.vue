a80b5e2cff05b7441884c91b4c21760093170b72
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2024-07-04 01:00:41 +0300

    Правка базовий шаблон додав VAppBar та перемикач теми

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
 <template>
   <v-app>
+    <v-app-bar name="app-bar" app>
+      <v-app-bar-title>Demo App</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn-icon class="mr-2" icon="mdi-theme-light-dark" flat @click="toggleTheme" />
+      </template>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
