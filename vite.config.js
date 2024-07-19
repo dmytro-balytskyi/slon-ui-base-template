c265a631127930f1a264efe63eeba04d27de79e2
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2024-07-19 03:15:03 +0300

    vite.config.js додав плагіни federation та topLevelAwait

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // Plugins
 import Vue from '@vitejs/plugin-vue'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import federation from '@originjs/vite-plugin-federation'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -19,12 +21,31 @@ export default defineConfig({
       styles: {
         configFile: 'src/styles/vuetify/settings.scss'
       }
+    }),
+    federation({
+      name: 'someshared',
+      filename: 're.js'
+      //shared: ['vue', 'vuetify']
+    }),
+    topLevelAwait({
+      promiseExportName: '__tla',
+      promiseImportName: (i) => `__tla_${i}`
     })
   ],
   define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    modulePreload: false,
+    minify: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: false
+      }
     }
   }
 })
