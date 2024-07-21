423895174aed08bf0c7fa3916a03cbbc33d78f78
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2024-07-22 01:56:44 +0300

    Видалив @chromatic-com/storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,9 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-essentials',
+    //'@storybook/addon-interactions',
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
-    //'@chromatic-com/storybook',
-    //'@storybook/addon-interactions'
   ],
   framework: {
     name: '@storybook/vue3-vite',
