264b1cb45059782a3682d38bfc8d56165d4dd5eb
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2025-01-23 22:25:11 +0200

    Оновив slon blueprint

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -15,9 +15,10 @@ const slonBlueprint = {
   aliases: {
     VBtnIcon: VBtn,
     SBtn: VBtn,
-    FBtn: VBtn
+    FBtn: VBtn,
   },
   defaults: {
+
     global: {
       rounded: 'md'
     },
@@ -35,7 +36,7 @@ const slonBlueprint = {
     },
 
     SBtn: {
-      class: 's-btn',
+      class: "s-btn",
       variant: 'text',
       rounded: 0,
       height: 48,
@@ -43,11 +44,12 @@ const slonBlueprint = {
         size: 32
       },
       style: {
-        fontSize: '16px'
-      }
+        fontSize: '16px',
+      },
     },
+
     FBtn: {
-      class: 'f-btn',
+      class: "f-btn",
       variant: 'tonal',
       rounded: 0,
       height: 48,
@@ -55,9 +57,10 @@ const slonBlueprint = {
         size: 32
       },
       style: {
-        fontSize: '16px'
-      }
+        fontSize: '16px',
+      },
     },
+
     VBtnToggle: {
       rounded: 0,
       color: 'success',
@@ -66,9 +69,11 @@ const slonBlueprint = {
         size: 32
       }
     },
+
     VBtnGroup: {
       rounded: 0
     },
+
     VFab: {
       color: 'primary',
       size: 'large',
@@ -77,18 +82,20 @@ const slonBlueprint = {
         size: 32
       }
     },
+
     VSpeedDial: {
       transition: 'slide-y-reverse-transition',
       scrollStrategy: 'close',
       VBtn: {
         width: 56,
         height: 56,
-        rounded: 'circle'
+        rounded: 'circle',
       },
       VIcon: {
         size: 32
       }
     },
+
     VRating: {
       activeColor: 'secondary',
       color: 'grey-darken-1',
@@ -99,68 +106,83 @@ const slonBlueprint = {
         class: ''
       }
     },
+
     VChip: {
       class: 'text-lowercase',
       closeIcon: 'mdi-close',
-      label: true
+      label: true,
     },
+
     VTab: {
       VIcon: {
         size: 32
       },
-      variant: 'text'
+      variant: 'text',
     },
+
     VTabs: {
       color: 'primary',
       sliderColor: 'primary',
-      mobileBreakpoint: 'md'
+      mobileBreakpoint: 'md',
     },
+
     VAvatar: {
       rounded: 'circle'
     },
+
     VBanner: {
       color: 'primary'
     },
+
     VCard: {
       rounded: 0
     },
+
     VCardTitle: {
       style: 'user-select: none;'
     },
+
     VToolbar: {
       VBtn: {
         color: null
       }
     },
+
     VBottomNavigation: {
       height: 60,
       VIcon: {
         size: 32
       },
-      grow: true
+      grow: true,
     },
+
     VSlider: {
       color: 'primary'
     },
+
     VTooltip: {
       openDelay: 3000,
-      location: 'top'
+      location: 'top',
     },
+
     VSnackbar: {
       rounded: 0,
       timeout: 4000
     },
+
     VList: {
       class: 'pa-0',
       style: 'user-select: none;',
       slim: true,
       rounded: 0,
       activatable: true,
-      VIcon: { size: 32 }
+      VIcon: { size: 32 }, // основний розмір іконок у VList
     },
+
     VListGroup: {
-      VIcon: { size: 24 }
+      VIcon: { size: 24 }, // іконки у VListGroup
     },
+
     VTextField: {
       VIcon: {
         size: 24
@@ -170,8 +192,9 @@ const slonBlueprint = {
       },
       variant: 'underlined',
       clearIcon: 'mdi-close',
-      active: true
+      active: true,
     },
+
     VTextarea: {
       VIcon: {
         size: 24
@@ -183,6 +206,7 @@ const slonBlueprint = {
       clearIcon: 'mdi-close',
       active: true
     },
+
     VNumberInput: {
       VBtn: {
         width: 16,
@@ -191,6 +215,7 @@ const slonBlueprint = {
         }
       }
     },
+
     VFileInput: {
       VLabel: {
         class: 'text-uppercase'
@@ -201,40 +226,43 @@ const slonBlueprint = {
       variant: 'underlined',
       active: true
     },
+
     VSelect: {
       VTextField: {
         VLabel: {
           class: 'text-uppercase'
         },
         VIcon: {
-          size: 24
-        }
+          size: 24 // Розмір іконок для VSelect
+        },
       },
       variant: 'underlined',
       clearIcon: 'mdi-close',
       active: true,
       VMenu: {
         VList: {
-          VIcon: { size: 24 }
+          VIcon: { size: 24 } // скидання розміру іконок у VMenu > VList
         }
       }
     },
+
     VCombobox: {
       VLabel: {
         class: 'text-uppercase'
       },
       VIcon: {
-        size: 24
+        size: 24 // розмір іконок для VCombobox
       },
       variant: 'underlined',
       clearIcon: 'mdi-close',
       active: true,
       VMenu: {
         VList: {
-          VIcon: { size: 24 }
+          VIcon: { size: 24 } // скидання розміру іконок у VMenu > VList
         }
       }
     },
+
     VAutocomplete: {
       VLabel: {
         class: 'text-uppercase'
@@ -247,15 +275,17 @@ const slonBlueprint = {
       active: true,
       VMenu: {
         VList: {
-          VIcon: { size: 24 }
+          VIcon: { size: 24 } // скидання розміру іконок у VMenu > VList
         }
       }
     },
+
     VSwitch: {
       color: 'success',
       density: 'comfortable',
       hideDetails: true
     },
+
     VCheckbox: {
       color: 'success',
       density: 'comfortable',
@@ -264,6 +294,7 @@ const slonBlueprint = {
         size: 24
       }
     },
+
     VRadio: {
       color: 'success',
       density: 'comfortable',
@@ -272,11 +303,16 @@ const slonBlueprint = {
         size: 24
       }
     },
+    
     VRadioGroup: {
       color: 'success',
       density: 'comfortable',
       hideDetails: true
-    }
+    },
+
+    VAppBar: {
+      height: 72,
+    },
   }
 }
 
