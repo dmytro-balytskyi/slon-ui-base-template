7344e3f85827728629ef04e5516c0970a8dac023
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2025-01-23 22:33:22 +0200

    Обгорнув сторінку в <error-boundary> компонент

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <script setup>
 import { useTitle } from '@vueuse/core'
+import ErrorBoundary from '@/components/ErrorBoundary.vue'
 
 useTitle('Назва проекту')
 </script>
 
 <template>
-  <v-container fluid>
-    <p>Заготовка сторінки проекту</p>
-    <div class="text-body-2 text-disabled pt-2">
-      <p>Клік на аватарі користувача перемикає тему dark/light</p>
-      <p>Не ускладнюй велосипед! Пам'ятай, що в результаті має бути проста shared сторінка</p>
-    </div>
-  </v-container>
+  <error-boundary>
+    <v-container fluid>
+      <p>Заготовка сторінки проекту</p>
+      <div class="text-body-2 text-disabled pt-2">
+        <p>Клік на аватарі користувача перемикає тему dark/light</p>
+        <p>Не ускладнюй велосипед! Пам'ятай, що в результаті має бути проста shared сторінка</p>
+      </div>
+    </v-container>
+  </error-boundary>
 </template>
 
 <style lang="scss"></style>
