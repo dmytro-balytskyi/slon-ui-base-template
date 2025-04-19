ca87b81b46d21a8ee271708e23274a8702866aca
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2025-04-19 21:54:18 +0300

    Правка конфіг vite.config.js, vitest.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,51 +9,65 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Vue({
-      template: { transformAssetUrls }
-    }),
-    Vuetify({
-      autoImport: {
-        labs: true
-      },
-      styles: {
-        configFile: 'node_modules/@slonbeton/slon-ui-blueprint-styles/src/styles/settings.scss'
-      }
-    }),
-    !process.env.STORYBOOKBUILD &&
-      federation({
-        name: 'someshared',
-        filename: 're.js'
-        //shared: ['vue', 'vuetify']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      Vue({
+        template: { transformAssetUrls }
       }),
-    !process.env.STORYBOOKBUILD &&
-      topLevelAwait({
-        promiseExportName: '__tla',
-        promiseImportName: (i) => `__tla_${i}`
-      })
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    modulePreload: false,
-    minify: false,
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        minifyInternalExports: false
+      Vuetify({
+        autoImport: {
+          labs: true
+        },
+        styles: {
+          configFile: 'node_modules/@slonbeton/slon-ui-blueprint-styles/src/styles/settings.scss'
+        }
+      }),
+      !process.env.VITEST &&
+        federation({
+          name: 'host-app',
+          remotes: {
+            shared: env.VITE_REMOTE_SHARED,
+            'concrete-orders': env.VITE_REMOTE_PAGE_CONCRETE_ORDERS
+          },
+          shared: ['vue', 'vuetify', '@apollo/client', '@vue/apollo-composable']
+        }),
+      !process.env.STORYBOOKBUILD &&
+        federation({
+          name: 'someshared',
+          filename: 're.js'
+          //shared: ['vue', 'vuetify']
+        }),
+      !process.env.STORYBOOKBUILD &&
+        topLevelAwait({
+          promiseExportName: '__tla',
+          promiseImportName: (i) => `__tla_${i}`
+        })
+    ],
+    define: { 'process.env': { ...process.env, ...env } },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    },
+    build: {
+      modulePreload: false,
+      minify: false,
+      sourcemap: true,
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          minifyInternalExports: false
+        }
       }
     }
   }
--- vitest.config.js
+++ vitest.config.js
@@ -2,21 +2,23 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      server: {
-        deps: {
-          inline: ['vuetify']
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        server: {
+          deps: {
+            inline: ['vuetify']
+          }
+        },
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+        transformMode: {
+          web: [/\.[jt]sx$/]
         }
-      },
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
       }
-    }
-  })
+    })
+  )
 )
