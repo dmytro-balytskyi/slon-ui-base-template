5704909bd690e4c699a1a812590ecdc2345f538d
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2026-02-21 22:20:35 +0200

    Видалення застарілих файлів

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { useTitle } from '@vueuse/core'
 import ErrorBoundary from '@/components/ErrorBoundary.vue'
 
-useTitle('Назва проекту')
+useTitle('нова сторінка')
 </script>
 
 <template>
@@ -11,7 +11,6 @@ useTitle('Назва проекту')
       <p>Заготовка сторінки проекту</p>
       <div class="text-body-2 text-disabled pt-2">
         <p>Клік на аватарі користувача перемикає тему dark/light</p>
-        <p>Не ускладнюй велосипед! Пам'ятай, що в результаті має бути проста shared сторінка</p>
       </div>
     </v-container>
   </error-boundary>
